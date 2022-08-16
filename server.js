a3242433e4cbe5ae5bf196249d55e523a2424012
Dominik Mrówka <dmrowka7@gmail.com>
2022-08-16 20:46:22 +0200

    Change port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults({
   noCors: true
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3131;
 server.use(middlewares);
 server.use(router);
 server.listen(port);
